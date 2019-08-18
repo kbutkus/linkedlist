814bac93357de68706cfd4a4b5e62525d79d3cc0
kbutkus@gmail.com <kbutkus@gmail.com>
2019-08-18 19:15:07 +0000

    initial commit

--- LinkedList.rb
+++ LinkedList.rb
@@ -37,15 +37,12 @@ class Stack
 end
 
 def reverse_list(list)
-    # ADD CODE HERE
     stack = Stack.new
     while list
-        # ADD CODE HERE
         stack.push(list.value)
         list = list.next_node
     end
 
-    # ADD CODE HERE
     return stack.data
 end
 
@@ -70,8 +67,24 @@ puts "-------"
 revlist = reverse_list(node3)
 
 print_values(revlist)
-# stack = Stack.new
-# stack.push(1)
-# stack.push(2)
-# stack.pop
-# stack.pop
+# Creates a new Stack object
+stack = Stack.new
+
+# Pushes (adds) the number 1 to the empty stack
+stack.push(1)
+
+# Pushes the number 2 to the TOP of the stack
+stack.push(2)
+
+# Pops (removes) the TOP number from the stack (2)
+puts stack.pop
+=> 2
+
+# Pops the remaining number from the stack (1)
+puts stack.pop
+=> 1
+
+# Tries to pop the TOP number from the stack,
+# but it is empty so it returns nil
+puts stack.pop
+=> nil
